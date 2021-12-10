0c886c002a5191f127bd8c69aea57adf59fff630
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-10 13:05:08 +0200

    initial commit

---
+++ src/test/java/abc/spring/jokes_app/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.spring.jokes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
